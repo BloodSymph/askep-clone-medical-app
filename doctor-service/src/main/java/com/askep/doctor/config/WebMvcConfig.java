553a38d661c64832f1928f18e6b84d471c7c16a5
BloodSymph <rubakharoma@gmail.com>
2024-12-16 20:18:42 +0200

    Test auth service

---
+++ doctor-service/src/main/java/com/askep/doctor/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.askep.doctor.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableCaching
+public class WebMvcConfig {
+
+    @Bean
+    public RestTemplate restTemplateConfig() {
+        return new RestTemplate();
+    }
+
+}
