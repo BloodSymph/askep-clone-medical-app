2ee4308a7576d081192dd061ade8f4d4145aa8e5
BloodSymph <rubakharoma@gmail.com>
2025-01-16 15:44:18 +0200

    Doctor service auth v6

--- doctor-service/src/main/java/com/askep/doctor/utils/security/CustomAuthorityConverter.java
+++ doctor-service/src/main/java/com/askep/doctor/utils/security/CustomAuthorityConverter.java
@@ -8,22 +8,19 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @UtilityClass
 public class CustomAuthorityConverter {
 
     public static List<? extends GrantedAuthority> customAuthorityConverter(Claims claims) {
-        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-        Object objectClaim = claims.get("roles");
-        List<?> claimList = ((ArrayList<?>) objectClaim);
-        for (int i = 0; i < claimList.size(); i++) {
-            var linkedHashMap = (LinkedHashMap<String, String>) claimList.get(i);
-            grantedAuthorities.add(
-                    new SimpleGrantedAuthority(linkedHashMap.get("authority"))
-            );
-        }
-        return grantedAuthorities;
+        List<?> claimList = (List<?>) claims.get("roles");
+
+        return claimList.stream()
+                .map(claim -> (LinkedHashMap<String, String>) claim)
+                .map(linkedHashMap -> new SimpleGrantedAuthority(linkedHashMap.get("authority")))
+                .collect(Collectors.toList());
     }
 
 }
